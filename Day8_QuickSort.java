d55c8b1804a9d6463b229af95c5894794f2d8715
wasiahmad <alexxwasi95@gmail.com>
2024-10-08 17:09:25 +0530

    Day8 Quick Sort

---
+++ Day8_QuickSort.java
@@ -0,0 +1,48 @@
+/**
+ * Day8_QuickSort
+ */
+public class Day8_QuickSort {
+
+    /* QUICKSORT */
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int pIdx = partition(arr, si, ei);
+        quickSort(arr, si, pIdx - 1);
+        quickSort(arr, pIdx + 1, ei);
+
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
+    // print array 
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 2, 9, 1, 7};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array");
+        printArr(arr);
+    }
+}
