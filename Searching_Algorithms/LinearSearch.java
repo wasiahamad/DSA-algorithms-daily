5edda6142af9b636937bf535455e42583af0e655
wasiahmad <alexxwasi95@gmail.com>
2024-10-28 15:15:25 +0530

    Day1 Linear Search

---
+++ Searching_Algorithms/LinearSearch.java
@@ -0,0 +1,22 @@
+package Searching_Algorithms;
+
+public class LinearSearch {
+    public static int linearSearch(int arr[], int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50,};
+        int target = 40;
+        int result = linearSearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element is not present in array");
+        } else {
+            System.out.println("Element is present at index " + result);
+        }
+    }
+}
